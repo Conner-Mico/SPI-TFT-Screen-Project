8272aec19be20148cb5fd6845e44a8bb730ca72d
cbwolfe94 <cbwolfe94@hotmail.com>
2024-08-08 16:13:05 -0500

    Add gpio types file

---
+++ src/app/gpio/gpio_types.h
@@ -0,0 +1,18 @@
+#ifndef GPIO_TYPES_H
+#define GPIO_TYPES_H
+
+typedef enum
+{
+    GPIO_START = 0,
+    GPIO_A = GPIO_START,
+    GPIO_B,
+    GPIO_C,
+    GPIO_D,
+    GPIO_E,
+    GPIO_F,
+    GPIO_G,
+    GPIO_NUM,
+    GPIO_INVALID,
+} GPIOx_t;
+
+#endif
