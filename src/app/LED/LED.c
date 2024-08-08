5c7f27a65dc674d5bbdaf84ce71e58b987e4ff20
Paul Vasquez <vasquep@alliancecoal.com>
2024-08-08 13:08:31 -0500

    first commit to new branch

--- src/app/LED/LED.c
+++ src/app/LED/LED.c
@@ -2,7 +2,19 @@
 #include "stm32l4xx_hal_gpio.h"
 
 // LED init function definition
+void LEDinit(void)
+{
+
+}
 
 // LED on function definition
+void LEDon(void)
+{
+
+}
 
 // LED off function definition
+void LEDoff(void)
+{
+
+}
--- src/app/LED/LED.h
+++ src/app/LED/LED.h
@@ -4,9 +4,12 @@
 #include "stm32l4xx_hal_gpio.h"
 
 // LED init function declaration
+void LEDinit(void);
 
 // LED on function declaration
+void LEDon(void);
 
 // LED off function declaration
+void LEDoff(void);
 
 #endif
