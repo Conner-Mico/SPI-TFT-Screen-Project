fd2389a43dd9fb64077d747018a732bbd7092d23
Paul Vasquez <vasquep@alliancecoal.com>
2024-08-08 13:31:50 -0500

    attempt at turning LED on

--- src/app/LED/LED.c
+++ src/app/LED/LED.c
@@ -1,20 +1,28 @@
 #include "LED.h"
 #include "stm32l4xx_hal_gpio.h"
 
+static GPIO_InitTypeDef blinkLEDPinInit = {
+                                            .Pin = GPIO_PIN_13,
+                                            .Mode = GPIO_MODE_OUTPUT_PP,
+                                            .Pull = GPIO_PULLDOWN,
+                                            .Speed = GPIO_SPEED_FREQ_HIGH,
+                                            .Alternate = 0, 
+                                        };
+
 // LED init function definition
 void LEDinit(void)
 {
-
+    HAL_GPIO_Init(&blinkLEDPinInit);
 }
 
 // LED on function definition
 void LEDon(void)
 {
-
+    HAL_GPIO_WritePin(GPIO_PIN_13, GPIO_PIN_SET); // Set pin high
 }
 
 // LED off function definition
 void LEDoff(void)
 {
-
+    HAL_GPIO_WritePin(GPIO_PIN_13, GPIO_PIN_RESET); // Set pin low
 }
--- src/app/application/application.c
+++ src/app/application/application.c
@@ -1,9 +1,16 @@
 #include "clock.h"
 #include "application.h"
+#include "LED.h"
 #include "stm32l4xx_hal.h"
 
 // Do a struct initialization with the approriate values for the pin
-static GPIO_InitTypeDef blinkLEDPinInit;
+static GPIO_InitTypeDef blinkLEDPinInit = {
+                                            .Pin = GPIO_PIN_13,
+                                            .Mode = GPIO_MODE_OUTPUT_PP,
+                                            .Pull = GPIO_PULLDOWN,
+                                            .Speed = GPIO_SPEED_FREQ_HIGH,
+                                            .Alternate = 0, 
+                                        };
 
 // application init function definition
 void applicationInit(void)
@@ -17,6 +24,6 @@ void applicationInit(void)
 void applicationRun(void)
 {
     while (1) {
-    
+        LEDon();
     }
 }
