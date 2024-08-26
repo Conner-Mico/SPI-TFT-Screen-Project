b466e3948e1cee0af791aaa259acf3da35119f03
mandelMakes <mandelwg@gmail.com>
2024-08-26 02:45:26 -0500

    Added a basic type to serve as representation of FT6202 IC and a type to handle configuration data

--- src/app/TFT-display/touch-screen-driver/FT6206_types.h
+++ src/app/TFT-display/touch-screen-driver/FT6206_types.h
@@ -4,4 +4,19 @@
 #include "stm32l4xx_hal.h"
 #include <stdint.h>
 
+typedef struct
+{
+
+} FT6206_ConfigRegister_Config_t;
+
+typedef struct
+{
+    FT6206_ConfigRegister_Config_t config;
+    uint16_t manufacturerID;
+    uint8_t deviceID;
+    uint8_t revision;
+
+} FT6206_t;
+
+
 #endif
