5fbb9ddfd6f790a7f89529eb3fe8cbaee79430a4
mandelMakes <mandelwg@gmail.com>
2024-08-26 03:31:10 -0500

    Added function prototypes for reading and writing config and data registers

--- src/app/TFT-display/touch-screen-driver/FT6206_driver.h
+++ src/app/TFT-display/touch-screen-driver/FT6206_driver.h
@@ -4,6 +4,10 @@
 #include "FT6206_types.h"
 
 void FT6206Init(const FT6206_t *FT6206_handle);
+void FT6202_configRegisterWrite(const FT6206_t *FT6206_handle);
+void FT6202_configRegisterRead(const FT6206_t *FT6206_handle);
+void FT6202_dataRegisterWrite(const FT6206_t *FT6206_handle);
+void FT6202_dataRegisterRead(const FT6206_t *FT6206_handle);
 
 
 #endif
